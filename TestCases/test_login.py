c1d76d0ebaa43933c482b8cbcf9b3d9bf5b552e5
tejaui <tejadigitalworld@gmail.com>
2025-11-09 10:04:32 +0530

    "Removed Logger capture"

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -1,19 +1,14 @@
 import pytest
-from Utilities.customlogger import Logger
 from PageObjects.LoginPage import LoginPage
 from Utilities.readproperties import ReadConfig
 from conftest import setup
-
-logger = Logger.setup_logger()
 
 @pytest.mark.sanity
 @pytest.mark.order(1)
 def test_001_homePageTitle(setup):
     driver = setup
     login_page = LoginPage(driver)
-    logger.info("Starting test: Login is attempting")
     login_page.login_check()
-    logger.info("Successfully logged in")
     assert True
 
 valid_username = ReadConfig.getUseremail()
